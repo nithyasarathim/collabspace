b071c3ecb1a1fc6eb447977bbc857db2ed1bfc6d
Nithyasarathi M <contact.nithyasarathi@gmail.com>
2025-09-15 10:48:17 +0530

    bug fix attempt-2

--- frontend/src/context/UserProvider.jsx
+++ frontend/src/context/UserProvider.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import {jwtDecode} from 'jwt-decode';
+import jwtDecode from 'jwt-decode';
 import UserContext from './UserContext';
 
 const UserProvider = ({ children }) => {
